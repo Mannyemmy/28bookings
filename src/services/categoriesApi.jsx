34684f396ade2d3684e4245b361a463b24b8a5f2
ore291 <oreoluwapadonu@gmail.com>
2022-05-24 00:28:36 +0100

    .

--- src/services/categoriesApi.jsx
+++ src/services/categoriesApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const categoriesApi = createApi({
   reducerPath: "categoriesApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL 
+    baseUrl: "https://pure-anchorage-21759.herokuapp.com/api/"
   }),
   endpoints: (builder) => ({
     getCategories: builder.query({
--- src/services/itemsApi.jsx
+++ src/services/itemsApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const itemsApi = createApi({
     reducerPath: 'items',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL
+    baseUrl: "https://pure-anchorage-21759.herokuapp.com/api/"
   }),
   endpoints: (builder) => ({
     getItemBySlug: builder.query({
