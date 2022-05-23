e6a1cf8e98dad09b441a7ee295402c37908af7c2
ore291 <oreoluwapadonu@gmail.com>
2022-05-23 23:28:20 +0100

    .

--- src/Api.js
+++ src/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // .. where we make our configurations
-        baseURL: 'http://127.0.0.1:8000/api/'
+        baseURL: process.env.BASE_URL || "http://localhost:8000/api/",
     });
 
 
--- src/services/itemsApi.jsx
+++ src/services/itemsApi.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const itemsApi = createApi({
     reducerPath: 'items',
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/",
+    baseUrl: process.env.BASE_URL || "http://localhost:8000/api/",
   }),
   endpoints: (builder) => ({
     getItemBySlug: builder.query({
