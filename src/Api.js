0677a3fab16ee889e32ce63992776940b7b52651
ore291 <oreoluwapadonu@gmail.com>
2022-05-24 00:34:26 +0100

    .

--- src/Api.js
+++ src/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // .. where we make our configurations
-        baseURL: 'https://pure-anchorage-21759.herokuapp.com/',
+        baseURL: 'https://pure-anchorage-21759.herokuapp.com/api/',
     });
 
 
